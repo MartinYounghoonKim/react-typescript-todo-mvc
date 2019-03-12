bddb3066c04e1d890b6a1b29bd3b66e650b91736
jeon-wonmin <wonmin.jeon@tmon.co.kr>
2019-03-12 13:22:16 +0900

    team4 initial commit :tada:

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,33 @@
-import React, {Component} from 'react';
+import * as React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
+class App extends React.Component {
+  public render() {
     return (
       <div className="todo-app">
         <header>
           <h1 className="todo-app__header">todos</h1>
           <input type="text"
-                 className="todo-app__new-todo"/>
-          <button/>
+            className="todo-app__new-todo" />
+          <button />
         </header>
         <div className="todo-app__main">
           <ul className="todo-list">
             <li className="todo-item">
-              <button className="toggle"/>
+              <button className="toggle" />
               <div className="todo-item__view">
                 <div className="todo-item__view__text">text</div>
-                <button className="todo-item__destroy"/>
+                <button className="todo-item__destroy" />
               </div>
               <input type="text"
-                     className="todo-item__edit"/>
+                className="todo-item__edit" />
             </li>
           </ul>
         </div>
         <footer className="footer">
           <span className="todo-count">
             <strong>1</strong>
-              <span>item</span>
+            <span>item</span>
             left
           </span>
           <ul className="todo-filters">
