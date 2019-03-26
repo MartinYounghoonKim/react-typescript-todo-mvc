fde24fa438856f3e94eb602582bd365f1bc5c2e6
Park Cheol Hyeon <pch33@tmon.co.kr>
2019-03-26 13:15:38 +0900

    컴포넌트 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,17 @@
 import * as React from 'react';
 import './App.css';
 
+import TodoFooter from './components/TodoFooter';
+import TodoHeader from './components/TodoHeader';
+import TodoMain from './components/TodoMain';
+
 class App extends React.Component {
   public render() {
     return (
       <div className="todo-app">
-        <header>
-          <h1 className="todo-app__header">todos</h1>
-          <input type="text"
-            className="todo-app__new-todo" />
-          <button />
-        </header>
-        <div className="todo-app__main">
-          <ul className="todo-list">
-            <li className="todo-item">
-              <button className="toggle" />
-              <div className="todo-item__view">
-                <div className="todo-item__view__text">text</div>
-                <button className="todo-item__destroy" />
-              </div>
-              <input type="text"
-                className="todo-item__edit" />
-            </li>
-          </ul>
-        </div>
-        <footer className="footer">
-          <span className="todo-count">
-            <strong>1</strong>
-            <span>item</span>
-            left
-          </span>
-          <ul className="todo-filters">
-            <li><a href="#none">All</a></li>
-            <li><a href="#none">Active</a></li>
-            <li><a href="#none">Completed</a></li>
-          </ul>
-        </footer>
+        <TodoHeader />
+        <TodoMain />
+        <TodoFooter />
       </div>
     );
   }
---
+++ src/components/TodoHeader.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+const TodoHeader = () => {
+  return (
+    <header>
+      <h1 className="todo-app__header">todos</h1>
+      <input type="text"
+        className="todo-app__new-todo" />
+      <button />
+    </header>
+  )
+}
+
+export default TodoHeader;
