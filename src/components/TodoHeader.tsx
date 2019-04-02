73a1b1d9e2697481038916b2fa02196347f2578a
jeon-wonmin <wonmin.jeon@tmon.co.kr>
2019-04-02 13:38:18 +0900

    헤더 작성 (CREATE)

--- src/components/TodoHeader.tsx
+++ src/components/TodoHeader.tsx
@@ -1,12 +1,38 @@
 import * as React from 'react';
 
 const TodoHeader = () => {
+  const [title, useTitle] = React.useState('');
+
+  function onChangeTodo(e: React.ChangeEvent<HTMLInputElement>) {
+    useTitle(e.target.value);
+  }
+
+  function onSubmitTodo(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    addTodo(title)
+  }
+
+  function addTodo(text: string) {
+    fetch('http://localhost:3000/todos', {
+      body: JSON.stringify({
+        completed: false,
+        title: text,
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      method: 'POST',
+    })
+  }
+
   return (
     <header>
-      <h1 className="todo-app__header">todos</h1>
-      <input type="text"
-        className="todo-app__new-todo" />
-      <button />
+      <form onSubmit={onSubmitTodo}>
+        <h1 className="todo-app__header">todos</h1>
+        <input type="text"
+          className="todo-app__new-todo" onChange={onChangeTodo} />
+        <button />
+      </form>
     </header>
   )
 }
