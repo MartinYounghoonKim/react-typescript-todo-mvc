f9bc5086103643a2874c12a81a739e1b0bfc5faa
martin <martin@tmon.co.kr>
2019-04-02 13:00:37 +0900

    Mobx 샘플 코드 수정

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,22 @@ import "./App.css";
 import Header from "./components/Header";
 import TodoList from "./components/TodoList";
 import Footer from "./components/Footer";
+import axios from "axios";
+import { observer } from 'mobx-react'
+import {action} from "mobx";
 
 interface IProps {
 
 }
 
+@observer
 class App extends React.Component<IProps> {
+  componentDidMount () {
+    axios.get("//localhost:2403/todos")
+      .then(res => {
+        console.log(res.data);
+      });
+  }
   render() {
     return (
       <div className="todo-app">
@@ -17,6 +27,19 @@ class App extends React.Component<IProps> {
         <Footer/>
       </div>
     );
+  }
+  @action.bound
+  onChange(event: MouseEvent) {
+    if (event.currentTarget) {
+      return;
+    }
+    const { name, value } = event.currentTarget;
+    this[name] = value;
+  }
+
+  onSubmit() {
+    const { email, password } = this
+    console.log('결과확인 : ', email, password);
   }
 }
 
